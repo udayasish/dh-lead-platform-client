a866315f9fed9243267b540ed16c68e27b52fb08
Udayasish <udayasish@gmail.com>
2026-07-26 18:04:53 +0530

    configure API URL in environment file and update conf.ts to use optional chaining

--- src/conf/conf.ts
+++ src/conf/conf.ts
@@ -1,5 +1,6 @@
 const conf = {
-  apiUrl: String(import.meta.env.VITE_API_URL),
+  apiUrl: import.meta.env.VITE_API_URL ?? "",
+  // apiUrl: String(import.meta.env.VITE_API_URL),
 };
 
 export default conf;
